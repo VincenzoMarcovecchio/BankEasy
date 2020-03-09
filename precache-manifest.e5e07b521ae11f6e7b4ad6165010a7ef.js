def22b8b91a66fdf3f14d0b35ead89cf7a6ff451
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2020-03-09 11:47:54 +0100

    Updates

---
+++ precache-manifest.e5e07b521ae11f6e7b4ad6165010a7ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e68b9b3ecd9517a72cff9ac20def54f",
+    "url": "/BankEasy/index.html"
+  },
+  {
+    "revision": "8b5126541c7c5466c13b",
+    "url": "/BankEasy/static/js/2.e7414c1e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/BankEasy/static/js/2.e7414c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3a5e24d7955ed33a64",
+    "url": "/BankEasy/static/js/main.f5ae9215.chunk.js"
+  },
+  {
+    "revision": "76d1f87c335344695b9b",
+    "url": "/BankEasy/static/js/runtime-main.367d59ea.js"
+  }
+]);
